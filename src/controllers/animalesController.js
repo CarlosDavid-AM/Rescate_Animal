7307bade1d4e4b9c0c2719d65b77123e452a29fc
Carlos David <apolayamendozacarlos@gmail.com>
2025-11-06 10:16:45 -0500

    Se crea el metodo y la ruta para guardar datos en Animales

--- src/controllers/animalesController.js
+++ src/controllers/animalesController.js
@@ -11,3 +11,57 @@ export const getAllAnimals = async (req, res) => {
     res.status(500).json({ message: "Error al obtener los datos" });
   }
 };
+
+export const saveAnimal = async (req, res) => {
+  const {
+    nombre,
+    especie,
+    edad,
+    estado_salud,
+    fecha_rescate,
+    adoptado,
+    ruta_archivo,
+    ruta_imagen,
+    observaciones,
+  } = req.body;
+
+  if (
+    !nombre ||
+    !especie ||
+    !edad ||
+    !estado_salud ||
+    !fecha_rescate ||
+    !adoptado ||
+    !ruta_archivo ||
+    !ruta_imagen ||
+    !observaciones
+  ) {
+    return res.status(400).json({ message: "Falta completar los campos" });
+  }
+
+  const sql = `INSERT INTO animales 
+  (nombre, especie, edad, estado_salud, fecha_rescate, adoptado, ruta_archivo, ruta_imagen, observaciones) 
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  try {
+    const [result] = await db.query(sql, [
+      nombre,
+      especie,
+      edad,
+      estado_salud,
+      fecha_rescate,
+      adoptado,
+      ruta_archivo,
+      ruta_imagen,
+      observaciones,
+    ]);
+
+    res.status(201).json({
+      id: result.insertId,
+      message: "Registrado correctamente",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Error Interno en el Servidor" });
+  }
+};
--- src/routes/animalesRouter.js
+++ src/routes/animalesRouter.js
@@ -4,5 +4,6 @@ import * as animalesController from "../controllers/animalesController.js";
 const router = Router();
 
 router.get("/", animalesController.getAllAnimals);
+router.post("/", animalesController.saveAnimal);
 
 export default router;
